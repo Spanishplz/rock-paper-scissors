c7d7c64f65fdd75c13c837cfe78765f98bfa9b8b
spanishplz <spanishplz@gmail.com>
2026-08-21 12:37:08 -0500

    Add a single round game

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function getComputerChoice() {
 // console.log(getComputerChoice()); // delete
 
 function getHumanChoice() {
-    let humanChoice = prompt("Rock, paper or scissors?");
+    let humanChoice = prompt("Rock, paper or scissors?", "rock"); //delete default
     let humanChoiceLower = humanChoice.toLowerCase();
     return humanChoiceLower;
     console.log(humanChoiceLower); // delete
@@ -29,7 +29,22 @@ function getHumanChoice() {
 function playRound() {
     let computer = getComputerChoice();
     let human = getHumanChoice();
-    console.log(`Computer choice: ${computer} and human choice: ${human}.`);
+    console.log(`Computer choice: ${computer} and human choice: ${human}.`); // delete
+    // 9 possibiities in total, 3 go on ties.
+    if ( computer === human) {
+        console.log(`It is a tie! Both chose: ${computer}`);
+        console.log(`Computer: ${computerScore} - Human: ${humanScore}.`);
+    } else if( computer === "rock" && human === "scissors"
+             || computer === "paper" && human === "rock"
+             || computer === "scissors" && human === "paper") {
+        console.log(`Computer wins with: ${computer}, as expected!.`);
+        ++computerScore;
+        console.log(`Computer: ${computerScore} - Human: ${humanScore}.`);
+    } else {                    // This accepts random values and gives you the win
+        console.log(`Human wins with: ${human}, as expected!.`);
+        ++humanScore;
+        console.log(`Computer: ${computerScore} - Human: ${humanScore}.`);
+    }
 }
 
 playRound();
