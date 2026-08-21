4d5c4cc6e71bafa151fbd0bae21b76ec1cc0bb3c
spanishplz <spanishplz@gmail.com>
2026-08-21 11:50:08 -0500

    Add player choice

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let choice = "";
     let result = Math.random()*(3 - 1 + 1) + 1;
@@ -10,8 +13,23 @@ function getComputerChoice() {
     } else {
         choice = "scissors";
     }
-    console.log(resultFloored); // delete
+    // console.log(resultFloored); // delete
     return choice;
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice()); // delete
+
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, paper or scissors?");
+    let humanChoiceLower = humanChoice.toLowerCase();
+    return humanChoiceLower;
+    console.log(humanChoiceLower); // delete
+}
+
+function playRound() {
+    let computer = getComputerChoice();
+    let human = getHumanChoice();
+    console.log(`Computer choice: ${computer} and human choice: ${human}.`);
+}
+
+playRound();
